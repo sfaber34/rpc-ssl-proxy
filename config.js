71bb2b310e1ee8ad3472037189f964700bbad5e6
root <root@ip-172-31-95-205.ec2.internal>
2025-05-28 23:18:34 +0000

    Forgot config edits

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
+const usdcAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
+const rpcFunderContractAddress = "0x291469065a4DDdE2CA9f6A53ab4Aa148B8e42f48";
 const backgroundTasksInterval = 10; //seconds
 
 export {
+  usdcAddress,
+  rpcFunderContractAddress,
   backgroundTasksInterval
 };
